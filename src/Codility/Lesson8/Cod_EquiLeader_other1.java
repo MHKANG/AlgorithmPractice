f1ad68fe188f88dde7165cfac5f02c74098863d2
강명훈 <lizingmaster@gmail.com>
2021-06-04 14:25:04 +0900

    2021-06-04
    Codility Algorithm
    - Change Lesson 8 Code

--- src/Codility/Lesson8/Cod_EquiLeader_other1.java
+++ src/Codility/Lesson8/Cod_EquiLeader_other1.java
@@ -1,54 +1,56 @@
 package Codility.Lesson8;
 
 import java.util.HashMap;
+import java.util.Vector;
 
-// Task : 22 Correctness: 40 Performance : 0
+//Task : 100 Correctness: 100 Performance : 100
+//reference : https://lipcoder.tistory.com/entry/EquiLeader-Codility
+
+
 public class Cod_EquiLeader_other1 {
 	public static void main(String[] args) {
 		int[] A = {4, 3, 4, 4, 4, 2};
 		
 		int answer = 0;
-        
-        for(int i = 0; i < A.length-1; i++){
-            HashMap<Integer, Integer> front = new HashMap<>();
-            HashMap<Integer, Integer> rear = new HashMap<>();
-
-            for(int j = 0; j <= i; j++){
-                if(!front.containsKey(A[j])){
-                    front.put(A[j], 1);
-                }else{
-                    front.put(A[j], front.get(A[j])+1);
-                }
-            }
-            for(int k = i+1; k < A.length; k++){
-                 if(!rear.containsKey(A[k])){
-                    rear.put(A[k], 1);
-                }else{
-                    rear.put(A[k], rear.get(A[k])+1);
-                }
-            }
-            
-            
-            int front_max = findMax(front);
-            int rear_max = findMax(rear);
-            
-            if(front_max == rear_max)
-                answer++;
-            
+       
+        int leader = 0, leaderCount = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < A.length; i++){
+           if(map.containsKey(A[i])) {
+        	   map.put(A[i], map.get(A[i])+1);
+           }else {
+        	   map.put(A[i], 1);
+           }
+           if(leaderCount < map.get(A[i])) {
+        	   leaderCount = map.get(A[i]);
+        	   leader = A[i];
+           }
 
         }
-        System.out.println(answer);
-		
-	}
-	 public static int findMax(HashMap<Integer, Integer>map){
-	        int max = -1;
-	        for(int key : map.keySet()){
-	        	if(map.get(key) > map.size()/2){
-	                max = key;
-	                break;
-	            }      
-	        }
+        Vector<Integer>record = new Vector<Integer>();
+        int cur = 0;
+        
+        for(int i = 0; i < A.length; i++) {
+     	   if(A[i] == leader) {
+     		   cur++;
+     	   }
+     	   record.add(cur);
+        }
+        
+        for(int i = 0; i < A.length-1; i++) {
+        	int left = record.elementAt(i); // 왼쪽 부분의 빈도수 leader 빈도수
+        	int right = record.lastElement() - left; // 오른쪽 부분의 leader 빈도수
+        	
+        	int limitEquiLeft = ((i + 1) / 2) + 1; 
+        	int limitEquiRight = ((A.length - (i + 1)) / 2) + 1; 
+        	
+        	if((left >= limitEquiLeft) && (right >= limitEquiRight)) { 
+        		answer++; 
+        	}
 
-	        return max;
-	    }
+        	
+        }
+        System.out.println(answer);	
+	}
+	
 }
